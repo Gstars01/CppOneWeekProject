3be0cf3a8d243e2570052e102f1c526d61c44325
HSM1025 <gkatjdah070@gmail.com>
2025-04-02 11:46:52 +0900

    Add Card Class

--- cppProject/cppProject.cpp
+++ cppProject/cppProject.cpp
@@ -4,6 +4,123 @@
 #include<ctime>
 using namespace std;
 
+class Card {
+protected:
+	int atk, hp;
+public:
+	Card(int a, int b) : atk(a), hp(b) {}
+	virtual int Skill() = 0;
+};
+
+class Warrior : public Card {
+public:
+	Warrior() : Card(6, 12) {}
+	int Skill() override {
+		// 데미지 감소 40%
+		// 쿨타임 3
+	}
+};
+
+class Paladin : public Card {
+public:
+	Paladin() : Card(5, 14) {}
+	int Skill() override {
+		// 데미지 감소 50% + 회복 2
+		// 쿨타임 4
+	}
+};
+
+class Archer : public Card {
+public:
+	Archer() : Card(7, 9) {}
+	int Skill() override {
+		// 3명 히트
+		// 쿨타임 2
+	}
+};
+
+class Hunter : public Card {
+public:
+	Hunter() : Card(8, 10) {}
+	int Skill() override {
+		// 1명 1.2배 나머지 0.5배 히트
+		// 쿨타임 3
+	}
+};
+
+class Thief : public Card {
+public:
+	Thief() : Card(7, 8) {}
+	int Skill() override {
+		// 데미지 2배, 기절 1턴
+		// 쿨타임 2
+
+	}
+};
+
+class Assassin : public Card {
+public:
+	Assassin() : Card(8, 7) {}
+	int Skill() override {
+		// 1명 1.5배
+		// 쿨타임 3
+	}
+};
+
+class Rogue : public Card {
+public:
+	Rogue() : Card(6, 9) {}
+	int Skill() override {
+		// 1명 0.9배 2회 타격
+		// // 쿨타임 2
+	}
+};
+
+class Priest : public Card {
+public:
+	Priest() : Card(4, 12) {}
+	int Skill() override {
+		// 4 회복
+		// 쿨타임 4
+	}
+};
+
+class Bard : public Card {
+public:
+	Bard() : Card(4, 10) {}
+	int Skill() override {
+		// 1명 공격력 +1 체력 +2
+		// 쿨타임 3
+	}
+};
+
+class Mage : public Card {
+public:
+	Mage() : Card(9, 6) {}
+	int Skill() override {
+		// 데미지 2배
+		// 쿨타임 3
+	}
+};
+
+class Sorcerer : public Card {
+public:
+	Sorcerer() : Card(8, 6) {}
+	int Skill() override {
+		// 3명 히트
+		// 쿨타임 3
+	}
+};
+
+class Necromancer : public Card {
+public:
+	Necromancer() : Card(8, 6) {}
+	int Skill() override {
+		// 공격력 1.5배 스텟 흡수
+		// 쿨타임 4
+	}
+};
+
 // card는 객체임 안쓸때는 빈 객체를 생성하여 0을 전달할것 
 void draw(int Ai_Lp, int Player_Lp, int card1, int card2, int card3, int card4, int card5, int card6) {
 	cout << "|----------------------------------------\n";
